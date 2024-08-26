d912129fc6c15e5fc1b0b51c8a794ba1714176bc
shazianaveed <160173580+shazianaveed@users.noreply.github.com>
2024-08-26 20:42:49 +0500

    Added 2nd assingment of js

---
+++ app.js
@@ -0,0 +1,21 @@
+
+// Legal & illegal
+//Q#1
+//  var name=" Soha ", age =  10 , goToSchool =  true
+
+//Q#2
+//Legal Variable Names
+// myVariable
+// number1
+// _temp
+// userName
+// totalAmount
+
+// //Illegal Variable Names
+// 1stVariable (Starts with a number)
+// @name (Contains a special character @)
+// var name (Contains a space)
+// class (Uses a reserved keyword in many languages)
+// total-amount (Contains a hyphen -)
+
+
